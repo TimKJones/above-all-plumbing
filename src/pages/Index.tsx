8010787d29b6cc7952b8a9556b8ea5e72fe3e85b
Tim <tjones4413@gmail.com>
2025-04-12 12:41:19 -0400

    Initial planning for service area section - identified integration point and available city pages

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,8 @@
-
 import React from 'react';
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import ServicesSection from '@/components/ServicesSection';
+import ServiceAreaSection from '@/components/ServiceAreaSection';
 
 const Index = () => {
   return (
@@ -10,6 +10,7 @@ const Index = () => {
       <Navbar />
       <Hero />
       <ServicesSection />
+      <ServiceAreaSection />
     </div>
   );
 };
